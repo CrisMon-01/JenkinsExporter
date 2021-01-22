dbe1a9073031b9817f3d50b5446388344e8ec22e
crismon-01 <cristianmonti1995@gmail.com>
2021-01-22 20:08:19 +0100

    add some logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import getpass
 import yaml
+import logging
 from lib import extractor
 from lib import uploader
 from lib import plugins
@@ -9,6 +10,18 @@ username = ''
 token = ''
 server = ''
 action = ''
+try:
+    os.remove("./logs/result.log")
+    fh = logging.FileHandler('./logs/result.log')
+except:
+    fh = logging.FileHandler('./logs/result.log')
+
+logger = logging.getLogger('MAIN')
+logger.setLevel(logging.DEBUG)
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
 
 if len(sys.argv)<=2:
     if '--auth-file' in sys.argv:
@@ -26,28 +39,37 @@ else:
     for arg in sys.argv:
         if '-n=' in arg:
             username = arg[3:]
-            print("[INFO] USER:"+username)
         if '-t=' in arg:
             token = arg[3:]
-            print("[INFO] TOKEN:"+token)
         if '-s=' in arg:
             server = arg[3:]
-            print("[INFO] SERVER:"+server)
         action = sys.argv[(len(sys.argv)-1)]
+
+logging.debug('USER: '+username)
+logging.debug('TOKEN: '+token)
+logging.debug('ACTION: '+action)
 
 print("[INFO] AUTH:"+username+" "+token+' @ SERVER: '+server)
 
 if action == 'download':
+    logger.debug("YOU WILL DOWNLOAD JOB'S CONFIG FILE")
     print("[INFO] YOU WILL DOWNLOAD JOB'S CONFIG FILE")
     extractor.extraction(username,token,server)
 else: 
     if action == 'upload':
+        logger.debug("YOU WILL UPLOAD JOB'S CONFIG FILE")
         print("[INFO] YOU WILL UPLOAD JOB'S CONFIG FILE")
         uploader.upload(username,token,server)
     else:
         if action == 'list-plugin':
+            logger.debug("LIST THE PLUGIN OF THE JENKINS @ "+server+" IN config/plugin.json")
             print("[INFO] I WILL LIST THE PLUGIN OF THE JENKINS @ "+server+" IN config/plugin.json")
-            plugins.makelist(username,token,server)
+            try:
+                plugins.makelist(username,token,server)
+                logger.debug("CHECK config/plugin.json")
+            except:
+                logger.error("ERROR IN MAKELIST ")
         else:
             print("[WARN] YOU HAVEN'T SPECIFY ACTION")
+            logger.error("NO PARAMS")
             sys.exit()
